b38b7cd2e57d381232e6b1f3586077c52a39294b
Roger Pimenta <roger.pimenta@live.com>
2021-09-21 10:21:03 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meal/Meals";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
