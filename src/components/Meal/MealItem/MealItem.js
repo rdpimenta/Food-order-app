3187d8a211185cf18be8078ccd5d740bef802ad0
Roger Pimenta <roger.pimenta@live.com>
2021-10-26 11:05:32 -0300

    Fetching data via http

--- src/components/Meal/MealItem/MealItem.js
+++ src/components/Meal/MealItem/MealItem.js
@@ -7,22 +7,22 @@ import CartContext from "../../../store/cart-context";
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const price = `$${props.meal.price.toFixed(2)}`;
+  const price = `$${props.price.toFixed(2)}`;
 
   const addToCartHandler = (amount) => {
     cartCtx.addItem({
-      id: props.meal.id,
-      name: props.meal.name,
+      id: props.id,
+      name: props.name,
       amount: amount,
-      price: props.meal.price,
+      price: props.price,
     });
   };
 
   return (
     <li className={classes.meal}>
       <div>
-        <h3>{props.meal.name}</h3>
-        <div className={classes.description}>{props.meal.description}</div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
